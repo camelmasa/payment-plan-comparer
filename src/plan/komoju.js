15852cdb1dd7756e000ae956ea89184f66d3e8e9
camelmasa <camelmasa@gmail.com>
2016-07-14 00:46:11 +0900

    Add payment methods

--- src/plan/komoju.js
+++ src/plan/komoju.js
@@ -9,6 +9,7 @@ export default class Komoju extends Plan {
     this.name           = "Komoju"
     this.serviceUrl     = "https://komoju.com"
     this.brands         = ["Visa", "MasterCard", "JCB", "American Express", "Diners Club"]
+    this.paymentMethods = ["CreditCard", "Konbini", "BankTransfer", "WebMoney", "NetCash", "BitCash", "PayEasy", "nanaco"]
     this.percentageFee  = 3.25
   }
 }
